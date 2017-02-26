ca63d66ec97958b498ef42dabd1be85aac6f9b27
PremiumSandwich <samneetdaudhar1996@gmail.com>
2017-02-26 16:30:19 +0000

    Added backgroundTemp, homeScreen, mainMenu, pauseMenu, changeScene Script
    backgroundTemp
    --added a temporary background
    
    homeScreen
    --added a home screen
    
    mainMenu
    --added a main menu
    
    pauseMenu
    --added a pause menu when you press escape or 'p' whilst the game is
    running
    
    changeScene
    --this script alllow you to change between scenes when a button is
    pressed

---
+++ TicTacToePlus/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class pauseMenu : MonoBehaviour {
+
+    public GameObject ui, game;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) {
+            togglePauseMenu();
+        }
+	}
+
+    public void togglePauseMenu() {
+
+        if (game.activeSelf == true)
+        {
+            ui.SetActive(true);
+            game.SetActive(false);
+        }
+        else
+        {
+            ui.SetActive(false);
+            game.SetActive(true);
+        }
+        
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+}
